47156a49f7265551a7c71d43d0149d0c99dc6c34
spectre-phantom <34074056+spectre-phantom@users.noreply.github.com>
2017-12-18 20:06:56 +0530

    Script 1 Completed

--- Script 1/countFiles.sh
+++ Script 1/countFiles.sh
@@ -7,12 +7,29 @@ if [ -n "$2" ]
 	if [[ $filetype != '.'* ]]
 		then
 		echo "Invalid input"
-		exit -1
+		exit 1
 	fi
 
 	echo''
+    temp=$(ls -A $path/*$filetype)
+    if [ $? -eq 1 ]
+    	then
+    	echo Error
+    	exit 1
+    fi
     ls -A $path/*$filetype|wc -l
- 
+    
 else
+	temp=$(ls -A $path)
+    if [ $? -eq 1 ]
+    	then
+    	echo Error
+    	exit 1
+    fi
 	ls -A $path|wc -l
 fi
+if [ $? -eq 1 ]
+    	then
+    	echo Error
+    	exit 1
+    fi
